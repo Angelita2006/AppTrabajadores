e4ac0e516a56331102f82566c70c38c6210c427a
María de los Ángeles <2903359@alu.murciaeduca.es>
2026-07-21 12:35:12 +0200

    updated database.py and made an alembic migration

--- api/backend/core/database.py
+++ api/backend/core/database.py
@@ -1,12 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# DATABASE_URL = "sqlite:///./database/test.db"
-# DATABASE_URL = "postgresql+psycopg2://postgres:postgres@127.0.0.1:5432/registrohorariosimple"
-DATABASE_URL = "postgresql+psycopg2://postgres:postgres@127.0.0.1:5432/registrohorariosimpledev"
-# DATABASE_URL = "mysql+pymysql://mariangeles:postgres@localhost:3306/registrohorariosimple"
-
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:postgres@127.0.0.1:5432/registrohorariosimpledev")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
