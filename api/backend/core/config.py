ec05f950ee45b53066199e6dc6b01070c7850b36
María de los Ángeles <2903359@alu.murciaeduca.es>
2026-09-22 13:44:30 +0200

    changed config.py for reading variables on .env

--- api/backend/core/config.py
+++ api/backend/core/config.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent.parent
 
 class Settings(BaseSettings):
     GEMINI_API_KEY: str = ""
